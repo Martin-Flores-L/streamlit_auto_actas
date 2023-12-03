8ab6d66f15619ae5ff4e253bbf50134cd60e5b94
Martin <52331147+Martin-Flores-L@users.noreply.github.com>
2023-12-03 05:31:16 +0000

    database

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ from supabase import create_client, Client
 
 # Initialize connection.
 # Uses st.cache_resource to only run once.
+@staticmethod
 @st.cache_resource
 def init_connection():
     url = st.secrets["SUPABASE_URL"]
