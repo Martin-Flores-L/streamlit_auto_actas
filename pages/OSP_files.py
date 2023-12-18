190c665907805d5ad17b260ed088eb672082b259
Martin <52331147+Martin-Flores-L@users.noreply.github.com>
2023-12-18 22:56:48 +0000

    Adding a new file for the new page

---
+++ pages/OSP_files.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import pytz
+from datetime import datetime
+
+#Set the page layout to wide
+st.set_page_config(layout="wide", initial_sidebar_state="auto")
+
+# Tittle
+st.title('OSP_FILES')
